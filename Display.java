25bacc58bd2453592fd4f1bc5c5a39f06c8c34b1
Roshan-Shah-7 <118169938+Roshan-Shah-7@users.noreply.github.com>
2023-06-20 12:36:02 +0545

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,6 @@
+class Display{
+public static void main(String[]args){
+System.out.println("*****Menu*****\n1.Add Number\n2.Subtract Number\n3.Multiply Number\n4.Divide Number");
+System.out.println("Enter Your Choice:");
+}
+}
